c0353f4a47e2cfe5efc8f818f98be6d2173ed499
ahgr3y <leehh97@gmail.com>
2025-02-17 17:13:35 +0800

    Implement cleanInput with tests

---
+++ repl_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := map[string]struct {
+		input    string
+		expected []string
+	}{
+		"empty input": {
+			input:    "",
+			expected: []string{},
+		},
+		"all caps": {
+			input:    "HELLO WORLD",
+			expected: []string{"hello", "world"},
+		},
+		"whitespace around": {
+			input:    "   Hello   World   ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			output := cleanInput(c.input)
+
+			if len(output) != len(c.expected) {
+				t.Errorf("unequal slice length: len(output): %d != len(c.expected): %d", len(output), len(c.expected))
+			} else {
+				for i := range c.expected {
+					if output[i] != c.expected[i] {
+						t.Errorf("output[%d]: %s != c.expected[%d]: %s", i, output[i], i, c.expected[i])
+					}
+				}
+			}
+
+		})
+	}
+}
